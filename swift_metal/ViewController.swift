f4dce47fbfbc600e96a4c457997ed2eae35eb45b
snowapril <sinjihng@gmail.com>
2024-03-24 20:09:11 +0900

    setup : initial commit for swift metal application

---
+++ swift_metal/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swift_metal
+//
+//  Created by snowapril on 3/24/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
