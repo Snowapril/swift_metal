45cfefb6581546a6d8a265fcb726b0c04936e2db
snowapril <sinjihng@gmail.com>
2024-04-15 18:32:25 +0900

    feat : move load/create mesh static methods to scene object loader

--- Renderer/SceneObject/SceneObjectLoader.swift
+++ Renderer/SceneObject/SceneObjectLoader.swift
@@ -7,14 +7,54 @@
 
 import MetalKit
 
-struct SceneObject {
-
-}
-
 class SceneObjectLoader {
+    static func loadMesh(url: URL, vertexDesc: MDLVertexDescriptor, device: MTLDevice) -> [Mesh] {
+        let bufferAllocator = MTKMeshBufferAllocator(device: device)
+        let mdlAsset = MDLAsset(url: url, vertexDescriptor: nil, bufferAllocator: bufferAllocator)
+        let textureLoader = MTKTextureLoader(device: device)
 
-    func loadSceneObject(objPath: String) -> SceneObject {
+        var meshes = [Mesh]()
+        for child in mdlAsset.childObjects(of: MDLObject.self) {
+            let assetMeshes = makeMeshes(
+                object: child,
+                vertexDesc: vertexDesc,
+                textureLoader: textureLoader,
+                device: device
+            )
+            meshes.append(contentsOf: assetMeshes)
+        }
+        return meshes
+    }
 
-        SceneObject()
+    static func makeMeshes(
+        object: MDLObject,
+        vertexDesc: MDLVertexDescriptor,
+        textureLoader: MTKTextureLoader,
+        device: MTLDevice
+    ) -> [Mesh] {
+        var meshes = [Mesh]()
+        if let mdlMesh = object as? MDLMesh {
+            let newMesh = Mesh(
+                mdlMesh: mdlMesh,
+                vertexDesc: vertexDesc,
+                textureLoader: textureLoader,
+                device: device
+            )
+            meshes.append(newMesh)
+        }
+
+        if object.conforms(to: MDLObjectContainerComponent.self) {
+            for child in object.children.objects {
+                let childMeshes = makeMeshes(
+                    object: object,
+                    vertexDesc: vertexDesc,
+                    textureLoader: textureLoader,
+                    device: device
+                )
+                meshes.append(contentsOf: childMeshes)
+            }
+        }
+
+        return meshes
     }
 }
